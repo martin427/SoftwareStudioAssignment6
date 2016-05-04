0e7c417d62041974b7cc2c1c539c8cd2dacfa66f
martin427 <martintw.kuo@gmail.com>
2016-05-04 17:53:33 +0800

    mimic from lab8

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 /**
@@ -8,16 +10,42 @@ import processing.core.PApplet;
 */
 public class Character {
 	
+	private float x, y, radius;
 	private MainApplet parent;
+	private String name;
+	private ArrayList<Character> targets;
 
-	public Character(MainApplet parent){
+	public Character(MainApplet parent, String name, float x, float y){
 
 		this.parent = parent;
+		this.name = name;
+		this.x = x;
+		this.y = y;
+		this.radius = 25;
 		
+		this.targets = new ArrayList<Character>();
 	}
 
 	public void display(){
-
+		this.parent.noStroke();
+		this.parent.fill(2, 247, 141);
+		this.parent.rect(x-name.length()*10, y-20, name.length()*20, 40, 12, 12, 12, 12);
+		
+		this.parent.textSize(26);
+		this.parent.fill(255);
+		this.parent.text(name, x-name.length()*10+5, y+10);
 	}
+	public void addTarget(Character target){ this.targets.add(target); }
 	
+	public ArrayList<Character> getTargets(){ return this.targets; }
+	
+	public float getX(){
+		return this.x;
+	}
+	public float getY(){
+		return this.y;
+	}
+	public float getRadius(){
+		return this.radius;
+	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,10 @@
 package main.java;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
 
 /**
 * This class is for sketching outcome using Processing
@@ -8,25 +12,82 @@ import processing.core.PApplet;
 */
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
-	private String path = "main/resources/";
-	private String file = "starwars-episode-1-interactions.json";
+	private String path = "resources/";
+	private String file = path + "starwars-episode-1-interactions.json";
 	
+	JSONObject data;
+	JSONArray nodes, links;
+	private ArrayList<Character> characters;
+	private int episode = 2;// which episode are you in
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
-
+		file = path + "starwars-episode-" + episode + "-interactions.json";
+		size(800, 800);
+		characters = new ArrayList<Character>();
 		size(width, height);
 		smooth();
 		loadData();
 		
 	}
+	public void keyPressed(){
+		if(keyCode==49){
+			episode = 1;
+			setup();
+		}else if(keyCode==50){
+			episode = 2;
+			setup();
+		}else if(keyCode==51){
+			episode = 3;
+			setup();
+		}else if(keyCode==52){
+			episode = 4;
+			setup();
+		}else if(keyCode==53){
+			episode = 5;
+			setup();
+		}else if(keyCode==54){
+			episode = 6;
+			setup();
+		}else if(keyCode==55){
+			episode = 7;
+			setup();
+		}
+		
+	}
 
 	public void draw() {
-
+background(255);
+		
+		stroke(60, 119, 119);
+		strokeWeight(4);
+		for(Character character: this.characters){
+			for(Character target: character.getTargets())
+				line(character.getX(), character.getY(), target.getX(), target.getY());
+		}
+		
+		for(Character character: this.characters)
+			character.display();
 	}
 
 	private void loadData(){
-
+		
+		data = loadJSONObject(file);
+		nodes = data.getJSONArray("nodes");
+		links = data.getJSONArray("links");
+		System.out.println("Number of nodes: " + nodes.size());
+		System.out.println("Number of links: " + links.size());
+		
+		for(int i=0; i<nodes.size(); i++){
+			JSONObject node = nodes.getJSONObject(i);
+			characters.add(new Character(this, node.getString("name"), random(100, 650), random(100, 650)));
+		}
+		for(int i=0; i<links.size(); i++){
+			JSONObject link = links.getJSONObject(i);
+			characters.get(link.getInt("source")).addTarget(characters.get(link.getInt("target")));
+		}
+		
+		
 	}
 
 }
